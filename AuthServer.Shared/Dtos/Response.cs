d20ce52a786811c60d4b37e100cd485038081fb8
seyyitatim <seyyitatim@gmail.com>
2022-12-12 14:42:12 +0300

    Add project files.

---
+++ AuthServer.Shared/Dtos/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T? Data { get; private set; }
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+        public ErrorDto? Error { get; private set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { StatusCode = statusCode, IsSuccessful = true };
+        }
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccessful = false };
+        }
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            return new Response<T> { Error = new ErrorDto(errorMessage, isShow), StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}
